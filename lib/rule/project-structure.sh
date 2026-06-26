3a7439b65e70e9e7321ac0f91dce9415bfb4b941
ByteBard <cwchen123@gmail.com>
2026-06-26 21:34:37 +0800

    Refactor a rule

---
+++ lib/rule/project-structure.sh
@@ -0,0 +1,40 @@
+lint ()
+{
+    EXIT_STATUS=0
+    __project="$1"
+    __no_directory=1
+    ERROR_MESSAGE=
+    for __directory in "$__project"/*; do
+        if ! [ -d "$__directory" ]; then continue; fi
+
+        __no_directory=0
+
+        case "$__directory" in
+            */bin|*/libexec) ;;
+            */lib|*/src) ;;
+            */share) ;;
+            */examples) ;;
+            */docs) ;;
+            */tools|*/scripts) ;;
+            */node_modules|*/local) ;;
+            *)
+                if [ -z "$ERROR_MESSAGE" ]; then
+                    ERROR_MESSAGE="Not a recommended directory: $__directory"
+                else
+                    ERROR_MESSAGE="${ERROR_MESSAGE}\nNot a recommended directory: $__directory"
+                fi
+                
+                EXIT_STATUS=1
+        esac
+    done
+
+    if [ "$EXIT_STATUS" = 1 ]; then
+        ERROR_SUMMARY="Not a recommended project structure"
+    fi
+
+    if [ "$__no_directory" = 1 ]; then
+        ERROR_MESSAGE="Flat project structure"
+        ERROR_SUMMARY="Flat project structure"
+        EXIT_STATUS=1
+    fi
+}
