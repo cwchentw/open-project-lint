398b78c619b026908c38ed3db9e7e33f0d790974
ByteBard <cwchen123@gmail.com>
2026-06-26 21:40:25 +0800

    Refactor a rule

---
+++ lib/rule/common-vcs.sh
@@ -0,0 +1,20 @@
+lint ()
+{
+    __project="$1"
+    EXIT_STATUS=0
+    __no_vcs=1
+    for __vcs_sub_directory in .git .hg .svn; do
+        __vcs_directory="$__project/$__vcs_sub_directory"
+
+        if [ -d "$__vcs_directory" ]; then
+            __no_vcs=0
+            break
+        fi
+    done
+
+    if [ "$__no_vcs" -ne 0 ]; then
+        ERROR_MESSAGE="No version source control detected. Git is recommended"
+        ERROR_SUMMARY="No version source control"
+        EXIT_STATUS=1
+    fi
+}
