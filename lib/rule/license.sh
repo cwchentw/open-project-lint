811c3a9cbf21fda6d8b2edf45f614f8146a9327e
ByteBard <cwchen123@gmail.com>
2026-06-26 17:14:39 +0800

    Refactor the codebase into rules as plugins

---
+++ lib/rule/license.sh
@@ -0,0 +1,11 @@
+lint ()
+{
+    EXIT_STATUS=0
+    __project="$1"
+    __license="$__project/LICENSE"
+    if ! [ -f "$__license" ]; then
+        ERROR_SUMMERY="No LICENSE file"
+        ERROR_MESSAGE="No LICENSE file: $__license"
+        EXIT_STATUS=1
+    fi
+}
---
+++ lib/rule/readme.sh
@@ -0,0 +1,45 @@
+lint ()
+{
+    EXIT_STATUS=0
+    __project="$1"
+    __no_readme=""
+    for __readme in "$__project"/README*; do
+        if ! [ -f "$__readme" ]; then continue; fi
+
+        __no_readme="$no_readme $readme"
+
+        __file_extension="${__readme##*.}"
+
+        case "$__file_extension" in
+        md|markdown|mdown|mkdn) ;;
+        rst) ;;
+        asciidoc|adoc|asc) ;;
+        mediawiki|wiki) ;;
+        org) ;;
+        textile) ;;
+        txt)
+            ERROR_SUMMARY="README file format not recommended"
+            ERROR_MESSAGE="README.txt is not recommended due to no syntax highlighting: $__readme"
+            EXIT_STATUS=1;;
+        "")
+            ERROR_SUMMARY="README file format not recommended"
+            ERROR_MESSAGE="${ERROR_MESSAGE}\nPlain README is not recommended due to no syntax highlighting: $__readme"
+            EXIT_STATUS=1;;
+        *)
+            ERROR_SUMMARY="Invalid README file format"
+            ERROR_MESSAGE="${ERROR_MESSAGE}\nInvalid README file extension: $__readme"
+        esac
+    done
+
+    if [ "$__no_readme" = "" ]; then
+        ERROR_SUMMARY="No REAEME.md file"
+        ERROR_MESSAGE="No README.md file: ${__project}/README.md"
+        EXIT_STATUS=1
+    fi
+
+    if [ $(echo "$__no_readme" | wc -w) -gt 1 ]; then
+        ERROR_SUMMARY="Duplicated README files"
+        ERROR_MESSAGE="${ERROR_MESSAGE}\nDuplicated README files: $__no_readme"
+        EXIT_STATUS=1
+    fi
+}
